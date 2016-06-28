3a41d7abeae2066549d599713c4cd7d38e71daef
Alex Armstrong <alehandrof@gmail.com>
2016-06-28 21:39:50 +0300

    Rework based on Jared Needell's fork
    https://github.com/jaredneedell/discourse-watch-category-mcneel/

--- plugin.rb
+++ plugin.rb
@@ -5,34 +5,33 @@
 # url: https://github.com/discourse/discourse-watch-category-mcneel
 
 module ::WatchCategory
-  def self.watch_category!
 
-    eresources_category = Category.find_by_slug("e-resources-committee")
-    eresources_group = Group.find_by_name("eresources")
+  def self.watch_by_group(category_slug, group_name)
+    category = Category.find_by_slug(category_slug)
+    group = Group.find_by_name(group_name)
 
-    unless eresources_category.nil? || eresources_group.nil?
-      eresources_group.users.each do |user|
-        watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
-        CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], eresources_category.id) unless watched_categories.include?(eresources_category.id)
-      end
-    end
+    return if category.nil? || group.nil?
 
-    digped_category = Category.find_by_slug("digital-pedagogy-committee")
-    digped_group = Group.find_by_name("digped")
-
-    return if digped_category.nil? || digped_group.nil?
-
-    digped_group.users.each do |user|
+    group.users.each do |user|
       watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
-      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], digped_category.id) unless watched_categories.include?(digped_category.id)
+      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], category.id) unless watched_categories.include?(category.id)
+      # Jared Needell's code
+      # CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], category.id) unless watched_categories.include?(category.id) || user.staged
     end
+  end
+
+  def self.watch_category!
+    WatchCategory.watch_by_group("digital-pedagogy-committee", "digped")
+    WatchCategory.watch_by_group("e-resources-committee", "eresources")
   end
 end
 
 after_initialize do
   module ::WatchCategory
     class WatchCategoryJob < ::Jobs::Scheduled
-      every 1.day
+      # every 1.day
+      # every 1.minute
+      every 6.hours
 
       def execute(args)
         WatchCategory.watch_category!
